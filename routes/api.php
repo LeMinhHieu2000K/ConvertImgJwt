63a41e10fbef67c65f22b517d57391936d70f62d
LeMinhHieu2000K <94779110+LeMinhHieu2000K@users.noreply.github.com>
2022-11-23 15:18:56 +0700

    first4

--- routes/api.php
+++ routes/api.php
@@ -14,21 +14,26 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
 Route::post("register", [ImgController::class, "postRegister"]);
 
 Route::post("login", [ImgController::class, "postLogin"]);
 
-Route::get("logout", [ImgController::class, "getLogout"]);
+Route::group(["middleware" => ["auth:api"]], function () {
+    Route::get("logout", [ImgController::class, "getLogout"]);
 
-Route::post('upload' , [ImgController::class, "postUploadImg"]);
+    Route::post('upload', [ImgController::class, "postUploadImg"]);
 
-Route::get('image-data' , [ImgController::class, "getImageData"]);
+    Route::get('image-data', [ImgController::class, "getImageData"]);
 
-Route::post('convert' , [ImgController::class, "convertImageData"]);
+    Route::post('convert', [ImgController::class, "convertImageData"]);
 
-Route::get('download' , [ImgController::class, "download_img"]);
+    Route::get('download', [ImgController::class, "download_img"]);
 
-Route::delete('delete/{id}',[ImgController::class, "getImageData"] );
+    Route::delete('delete/{id}', [ImgController::class, "getImageData"]);
+});
+
+
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
